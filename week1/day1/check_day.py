de4a53d71a70596097d6f3b61fb9ad4521bf7830
OmarInCS <omarincs@gmail.com>
2020-07-04 23:07:40 +0300

    input, output, expressions, if-statment

---
+++ week1/day1/check_day.py
@@ -0,0 +1,10 @@
+
+day = eval(input("Enter a day number: "))
+
+if day >= 1 and day <= 5:
+    print("Word-day")
+elif day == 6 or day == 7:
+    print("off-day")
+else:
+    print("Invalid Day Number")
+
---
+++ week1/day1/compute_bmi.py
@@ -0,0 +1,5 @@
+
+weight, height = eval(input("Enter weight and height: "))
+bmi = weight / height ** 2
+
+print("BMI is:", round(bmi, 2))
