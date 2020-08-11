f6582454326787f784ec05dc5a6fc74fd73d9d7a
OmarInCS <omarincs@gmail.com>
2020-08-11 20:54:44 +0300

    if statement

---
+++ week1/day2/compute_bmi.py
@@ -0,0 +1,15 @@
+
+weight, height = eval(input("Enter your weight and height: "))
+bmi = weight / height ** 2
+
+print("BMI is:", round(bmi, 2))
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
+
