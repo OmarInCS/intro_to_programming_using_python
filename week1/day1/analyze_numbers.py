e49aaeafb102d0143dd582a1327b14f08e94f31e
OmarInCS <omarincs@gmail.com>
2020-08-24 14:59:09 +0300

    reset branch

--- week1/day1/analyze_numbers.py
+++
@@ -1,7 +0,0 @@
-
-n1, n2, n3 = eval(input("Enter three numbers: "))
-
-avg = (n1 + n2 + n3) / 3
-print("The Max is:", max(n1, n2, n3))
-print("The Min is:", min(n1, n2, n3))
-print("The Average is:", avg)
--- week3/day1/compute_area.py
+++
@@ -1,9 +0,0 @@
-
-from week3.day1.rectangle import Rectangle
-
-w, h = eval(input("Enter width and length: "))
-r1 = Rectangle(w, h)
-print("Area:", r1.get_area())
-
-r2 = Rectangle(3, 7)
-print("Area:", r2.get_area())
