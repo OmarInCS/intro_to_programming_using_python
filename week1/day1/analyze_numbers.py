72e13b432a4b9d9f592b4ca06776b69567a20502
OmarInCS <omarincs@gmail.com>
2020-08-24 22:18:35 +0300

    variables and datatypes

---
+++ week1/day1/analyze_numbers.py
@@ -0,0 +1,8 @@
+
+n1, n2, n3 = eval(input("Enter three marks: "))
+
+avg = (n1 + n2 + n3) / 3
+
+print("The Max Mark:", max(n1, n2, n3))
+print("The Min Mark:", min(n1, n2, n3))
+print("The Average:", avg)
---
+++ week1/day1/student_mark.py
@@ -0,0 +1,6 @@
+
+mark = eval(input("Enter your mark: "))
+final_mark = eval(input("Enter the exam final mark: "))
+
+pct = mark / final_mark * 100
+print("Percentage: ", pct, "%")
