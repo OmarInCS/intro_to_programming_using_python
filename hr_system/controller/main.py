56f650f09b32e933b6dc58d379495cf76a376adc
OmarInCS <omarincs@gmail.com>
2020-09-23 07:11:42 +0300

    HR System - Day 3

--- hr_system/controller/main.py
+++ hr_system/controller/main.py
@@ -1,5 +1,6 @@
 
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
 from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
@@ -16,6 +17,10 @@ class MainWindow(QWidget, Ui_Form):
 
         self.emps = Employee.get_all_emps()
         self.load_emps()
+
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+        self.le_search.textChanged.connect(self.filter_emps_by_name)
+        self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
         print()
 
     def load_depts(self):
@@ -23,12 +28,40 @@ class MainWindow(QWidget, Ui_Form):
         self.cb_depts.addItems(dept_names)
 
     def load_emps(self):
-
+        self.tb_emps.setRowCount(0)
         for i, e in enumerate(self.emps):
             self.tb_emps.insertRow(i)
             for j, value in enumerate(e.__dict__.values()):
                 self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
 
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            d = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != d.dept_id:
+                    self.tb_emps.hideRow(i)
+
+    def filter_emps_by_name(self):
+        search_term = self.le_search.text().lower()
+        self.load_emps()
+        if search_term != "":
+            for i, e in enumerate(self.emps):
+                if not e.emp_name.lower().startswith(search_term):
+                    self.tb_emps.hideRow(i)
+
+    def show_add_dept_dialog(self):
+        dialog = loadUi("../view/add_dept.ui")
+        locs = {str(d.loc_id) for d in self.depts}
+        dialog.cb_locs.addItems(locs)
+        choice = dialog.exec()
+
+        if choice == 1:
+            d1 = Department(dialog.le_dept_id.text(),
+                            dialog.le_dept_name.text(),
+                            dialog.cb_locs.currentText())
+            self.cb_depts.addItem(d1.dept_name)
+            self.depts.append(d1)
 
 
 app = QApplication([])
