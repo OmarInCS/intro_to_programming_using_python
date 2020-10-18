ba3c3a7c272dd957f2664f346f0ffb05f6990a01
OmarInCS <omarincs@gmail.com>
2020-10-18 10:31:36 +0300

    HR System - 2

--- hr_system/controller/main.py
+++ hr_system/controller/main.py
@@ -1,6 +1,8 @@
 
 from PyQt5.QtWidgets import *
 
+from hr_system.model.department import Department
+from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
 
 
@@ -9,6 +11,43 @@ class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         QWidget.__init__(self)
         self.setupUi(self)
+
+        self.depts = Department.get_all_depts()
+        self.load_depts()
+
+        self.emps = Employee.get_all_emps()
+        self.load_emps()
+
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+        self.le_search.textChanged.connect(self.filter_emps_by_name)
+        print()
+
+    def load_depts(self):
+        dept_names = [d.dept_name for d in self.depts]
+        self.cb_depts.addItems(dept_names)
+
+    def load_emps(self):
+        self.tb_emps.setRowCount(0)
+        for i, e in enumerate(self.emps):
+            self.tb_emps.insertRow(i)
+            for j, value in enumerate(e.__dict__.values()):
+                self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            d = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != d.dept_id:
+                    self.tb_emps.hideRow(i)
+
+    def filter_emps_by_name(self):
+        self.load_emps()
+        search_term = self.le_search.text().lower()
+        if search_term != "":
+            for i, e in enumerate(self.emps):
+                if not e.emp_name.lower().startswith(search_term):
+                    self.tb_emps.hideRow(i)
 
 
 app = QApplication([])
