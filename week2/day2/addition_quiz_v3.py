1a61b863ef44dd03a92988e9a3c7eeadcdf9dfcc
OmarInCS <omarincs@gmail.com>
2020-10-19 17:39:11 +0300

    for loop

---
+++ week2/day2/addition_quiz_v3.py
@@ -0,0 +1,18 @@
+
+from random import randint
+
+n = eval(input("Enter number of questions: "))
+score = 0
+
+for i in range(n):
+    x = randint(0, 10)
+    y = randint(0, 10)
+    answer = eval(input(f"What's {x} + {y} ? "))
+
+    if answer == x+y:
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong")
+
+print("Score:", score)
