835418e6a10c995c1479f797334ebac5f70e1491
OmarInCS <omarincs@gmail.com>
2020-11-02 21:52:43 +0300

    load data

--- hr_system/controller/main.py
+++ hr_system/controller/main.py
@@ -1,7 +1,9 @@
 
 from PyQt5.QtWidgets import *
 
+from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
+from hr_system.model.department import Department
 
 
 class MyWidget(QWidget, Ui_Form):
@@ -9,6 +11,25 @@ class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.depts = Department.get_all_depts()
+        self.load_depts()
+
+        self.emps = Employee.get_all_emps()
+        self.load_emps()
+
+        print()
+
+    def load_depts(self):
+        names = [d.dept_name for d in self.depts]
+        self.cb_depts.addItems(names)
+
+    def load_emps(self):
+
+        for i, e in enumerate(self.emps):
+            self.tb_emps.insertRow(i)
+            for j, value in enumerate(e.__dict__.values()):
+                self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
 
 
 app = QApplication([])
