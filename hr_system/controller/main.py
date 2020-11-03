2f758c91a01ef1109bb6545d4dfef3d159b82d4a
OmarInCS <omarincs@gmail.com>
2020-11-03 22:00:51 +0300

    filters & add dept dialog

--- hr_system/controller/main.py
+++ hr_system/controller/main.py
@@ -1,5 +1,6 @@
 
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
 from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
@@ -18,6 +19,9 @@ class MyWidget(QWidget, Ui_Form):
         self.emps = Employee.get_all_emps()
         self.load_emps()
 
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+        self.le_search.textChanged.connect(self.filter_emps_by_name)
+        self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
         print()
 
     def load_depts(self):
@@ -25,11 +29,33 @@ class MyWidget(QWidget, Ui_Form):
         self.cb_depts.addItems(names)
 
     def load_emps(self):
-
+        self.tb_emps.setRowCount(0)
         for i, e in enumerate(self.emps):
             self.tb_emps.insertRow(i)
             for j, value in enumerate(e.__dict__.values()):
                 self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            dept = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != dept.dept_id:
+                    self.tb_emps.hideRow(i)
+
+    def filter_emps_by_name(self):
+        self.load_emps()
+        query = self.le_search.text().upper()
+        if query != "":
+            for i, e in enumerate(self.emps):
+                if not e.emp_name.upper().startswith(query):
+                    self.tb_emps.hideRow(i)
+
+    def show_add_dept_dialog(self):
+        dialog = loadUi("../view/add_dept.ui")
+        locs = {str(d.loc_id) for d in self.depts}
+        dialog.cb_locs.addItems(locs)
+        dialog.exec()
 
 
 app = QApplication([])
