90a493fa13b9dbe0d1008ca2ae04abd56ad26700
OmarInCS <omarincs@gmail.com>
2020-11-08 21:55:13 +0300

    add, delete and export

--- hr_system/controller/main.py
+++ hr_system/controller/main.py
@@ -1,6 +1,7 @@
 
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
+from datetime import date
 
 from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
@@ -24,6 +25,9 @@ class MyWidget(QWidget, Ui_Form):
         self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
         self.bt_exit.clicked.connect(app.exit)
         self.bt_del_dept.clicked.connect(self.delete_dept)
+        self.bt_add_emp.clicked.connect(self.show_add_emp_dialog)
+        self.bt_del_emp.clicked.connect(self.delete_emp)
+        self.bt_export.clicked.connect(self.export_data)
         print()
 
     def load_depts(self):
@@ -73,6 +77,55 @@ class MyWidget(QWidget, Ui_Form):
             self.cb_depts.removeItem(idx)
             d = self.depts.pop(idx - 1)
             d.delete_from_db()
+
+    def show_add_emp_dialog(self):
+        dialog = loadUi("../view/add_emp.ui")
+        dialog.de_hire_date.setDate(date.today())
+        jobs = {str(e.job_id) for e in self.emps}
+        dialog.cb_jobs.addItems(jobs)
+        depts = [d.dept_name for d in self.depts]
+        dialog.cb_depts.addItems(depts)
+        choice = dialog.exec()
+
+        if choice == 1:
+            idx = dialog.cb_depts.currentIndex()
+            e = Employee(dialog.le_emp_id.text(),
+                         dialog.le_emp_name.text(),
+                         dialog.le_email.text(),
+                         dialog.de_hire_date.date().toString("yyyy-MM-dd"),
+                         dialog.cb_jobs.currentText(),
+                         dialog.le_salary.text(),
+                         self.depts[idx - 1].dept_id)
+            self.emps.append(e)
+            self.load_emps()
+            e.save_to_db()
+
+    def delete_emp(self):
+        idx = self.tb_emps.currentRow()
+        if idx != -1:
+            self.tb_emps.removeRow(idx)
+            e = self.emps.pop(idx)
+            e.delete_from_db()
+
+    def export_data(self):
+        info = QFileDialog().getSaveFileName(filter="CSV File(*.csv)")
+
+        if info[0] != "":
+            with open(info[0], "w") as file:
+                row_count = self.tb_emps.rowCount()
+                col_count = self.tb_emps.columnCount()
+
+                for i in range(col_count):
+                    header = self.tb_emps.horizontalHeaderItem(i).text()
+                    file.write(header + ",")
+                file.write("\n")
+
+                for i in range(row_count):
+                    if not self.tb_emps.isRowHidden(i):
+                        for j in range(col_count):
+                            data = self.tb_emps.item(i, j).text()
+                            file.write(data + ",")
+                        file.write("\n")
 
 
 app = QApplication([])
