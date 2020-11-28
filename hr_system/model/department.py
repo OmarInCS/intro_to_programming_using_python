44c0e4d060f011af5a3c5a8ad0f796656fbb3615
OmarInCS <omarincs@gmail.com>
2020-11-28 22:18:03 +0300

    fix database path

--- hr_system/model/department.py
+++ hr_system/model/department.py
@@ -14,8 +14,8 @@ class Department:
 
     @staticmethod
     def get_all_depts():
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        with connect(f"{basedir}\\hr.db") as conn:
+        db_path = os.path.abspath(os.getcwd() + "/../model/hr.db")
+        with connect(db_path) as conn:
             cur = conn.cursor()
             sql = "SELECT * FROM departments"
             result = cur.execute(sql).fetchall()
@@ -24,16 +24,16 @@ class Department:
             return result
 
     def save_to_db(self):
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        with connect(f"{basedir}\\hr.db") as conn:
+        db_path = os.path.abspath(os.getcwd() + "/../model/hr.db")
+        with connect(db_path) as conn:
             cur = conn.cursor()
             sql = "INSERT INTO departments VALUES (:dept_id, :dept_name, :loc_id)"
             cur.execute(sql, self.__dict__)
             conn.commit()
 
     def delete_from_db(self):
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        with connect(f"{basedir}\\hr.db") as conn:
+        db_path = os.path.abspath(os.getcwd() + "/../model/hr.db")
+        with connect(db_path) as conn:
             cur = conn.cursor()
             sql = "DELETE FROM departments WHERE department_id = :dept_id"
             cur.execute(sql, self.__dict__)
