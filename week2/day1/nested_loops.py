981b4674f3d5693e165e92961bb6701a6ff108c0
OmarInCS <omarincs@gmail.com>
2021-01-10 21:49:48 +0300

    nested loops

---
+++ week2/day1/nested_loops.py
@@ -0,0 +1,15 @@
+# break/continue
+# for y in range(1, 11):
+#     for x in range(1, 11):
+#         if x == 5 and y == 2:
+#             break
+#         print(x, end="\t")
+#     print()
+
+for y in range(1, 11):
+    for x in range(1, 11):
+        if x == y:
+            print("#", end="\t")
+            continue
+        print(x, end="\t")
+    print()
