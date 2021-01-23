fce58774ec27ef58c8f78d55c5faf1d61bdee639
OmarInCS <omarincs@gmail.com>
2021-01-23 21:25:22 +0300

    inheritance

---
+++ week3/day4/shape.py
@@ -0,0 +1,14 @@
+
+from termcolor import cprint
+
+
+class Shape:
+
+    def __init__(self, color):
+        self.color = color
+
+    def get_area(self):
+        pass
+
+    def print_with_color(self):
+        cprint(self, self.color)
---
+++ week3/day4/test_shapes.py
@@ -0,0 +1,12 @@
+
+from week3.day4.rectangle import Rectangle
+from week3.day4.circle import Circle
+
+r1 = Rectangle(4, 5, "blue")
+c1 = Circle(3, "red")
+
+r1.print_with_color()
+c1.print_with_color()
+
+print(r1.__dict__)
+print(r1.__class__)
