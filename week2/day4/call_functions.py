c01a6c5a96413c24cbe560a1735882ee399d9355
OmarInCS <omarincs@gmail.com>
2021-02-17 21:37:33 +0300

    functions

---
+++ week2/day4/call_functions.py
@@ -0,0 +1,15 @@
+
+from week2.day4.my_functions import *
+
+# w, h = eval(input("Enter your weight and height: "))
+#
+# bmi = get_bmi(w, h)
+# status = get_status(bmi)
+#
+# print(status, bmi)
+
+print(get_pct(23, 25))
+print(get_pct(23))
+print(get_pct(23, 30))
+print(get_pct())
+print(get_pct(final_mark=50))
