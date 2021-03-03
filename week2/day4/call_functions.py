1ce40f04b55a0a15d44730f4858c4797f7a9f02c
OmarInCS <omarincs@gmail.com>
2021-03-03 19:31:15 +0300

    functions

---
+++ week2/day4/call_functions.py
@@ -0,0 +1,14 @@
+
+from week2.day4.my_functions import *
+
+# w, h = eval(input("Enter weight and height: "))
+# bmi = calc_bmi(w, h)
+# status = get_status(bmi)
+#
+# print(status, bmi)
+
+print(calc_pct(23, 25))
+print(calc_pct(23))
+print(calc_pct(23, 30))
+print(calc_pct())
+print(calc_pct(f_mark=30))
---
+++ week2/day4/global_variables.py
@@ -0,0 +1,12 @@
+
+x = 5
+
+
+def do_something():
+    global x
+    x = 15
+    print(x)
+
+
+do_something()
+print(x)
