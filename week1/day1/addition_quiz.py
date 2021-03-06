bf147992dd2e2b2eae523860b45e56de01f552a3
OmarInCS <omarincs@gmail.com>
2021-03-06 20:40:41 +0300

    variables, datatypes, common functions, if-statement

---
+++ week1/day1/addition_quiz.py
@@ -0,0 +1,13 @@
+from random import randint
+
+x = randint(0, 10)
+y = randint(0, 10)
+
+# print("What's", x, "+", y, "?")
+# print(f"What's {x} + {y} ? ")
+
+answer = eval(input(f"What's {x} + {y} ? "))
+if answer == x+y:
+    print("Correct Answer")
+else:
+    print("Wrong Answer")
