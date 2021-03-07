9b27ff3c41011313fdd1435062ad5e0d8016f120
OmarInCS <omarincs@gmail.com>
2021-03-07 21:44:28 +0300

    HR System - part 2

--- hr_system/controller/app.py
+++ hr_system/controller/app.py
@@ -1,9 +1,39 @@
 
 from PyQt5.QtWidgets import *
 
+from hr_system.model.department import Department
+from hr_system.model.employee import Employee
+from hr_system.view.main_window import Ui_Form
+
+
+class MainWindow(QWidget, Ui_Form):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.depts = Department.get_all_depts()
+        self.load_depts()
+
+        self.emps = Employee.get_all_emps()
+        self.load_emps()
+
+
+    def load_depts(self):
+        names = [d.dept_name for d in self.depts]
+        self.cb_depts.addItems(names)
+
+    def load_emps(self):
+
+        for i, e in enumerate(self.emps):
+            self.tb_emps.insertRow(i)
+            for j, value in enumerate(e.__dict__.values()):
+                self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
+
+
 
 
 app = QApplication([])
-window = QWidget()
+window = MainWindow()
 window.show()
 app.exec()
---
+++ hr_system/model/department.py
@@ -0,0 +1,23 @@
+from sqlite3 import connect
+
+
+class Department:
+
+    def __init__(self, dept_id, dept_name, loc_id):
+        self.dept_id = dept_id
+        self.dept_name = dept_name
+        self.loc_id = loc_id
+
+    def __repr__(self):
+        return self.dept_name
+
+    @staticmethod
+    def get_all_depts():
+        db_path = "D:\\Abadnet\\Python\\Group20210207\\week4\\day3\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "SELECT * FROM departments"
+            result = cur.execute(sql).fetchall()
+            result = [Department(*row) for row in result]
+
+            return result
