d8c82316113352dd2659140ac026dfc669b8ed7e
OmarInCS <omarincs@gmail.com>
2021-03-10 21:48:42 +0300

    HR System

--- hr_system/model/employee.py
+++ hr_system/model/employee.py
@@ -25,3 +25,20 @@ class Employee:
             result = [Employee(*row) for row in result]
 
             return result
+
+    def save_to_db(self):
+        db_path = "D:\\Abadnet\\Python\\Group20210207\\week4\\day3\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "INSERT INTO employees (employee_id, last_name, email, hire_date, job_id, salary, department_id) " \
+                  "VALUES (:emp_id, :emp_name, :email, :hire_date, :job_id, :salary, :dept_id)"
+            cur.execute(sql, self.__dict__)
+            conn.commit()
+
+    def delete_from_db(self):
+        db_path = "D:\\Abadnet\\Python\\Group20210207\\week4\\day3\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "DELETE FROM employees WHERE employee_id = :emp_id"
+            cur.execute(sql, self.__dict__)
+            conn.commit()
