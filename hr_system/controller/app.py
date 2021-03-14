557acd5b4a7c67a281538da0bad7af80373f20d7
OmarInCS <omarincs@gmail.com>
2021-03-14 17:49:45 +0300

    HR System

---
+++ hr_system/controller/app.py
@@ -0,0 +1,46 @@
+
+from PyQt5.QtWidgets import *
+
+from hr_system.model.department import Department
+from hr_system.model.employee import Employee
+from hr_system.view.main_window import Ui_Form
+
+
+class MainWindow(QWidget, Ui_Form):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setupUi(self)
+
+        self.depts = Department.get_all_depts()
+        self.load_depts()
+
+        self.emps = Employee.get_all_emps()
+        self.load_emps()
+
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+
+    def load_depts(self):
+        names = [d.dept_name for d in self.depts]
+        self.cb_depts.addItems(names)
+
+    def load_emps(self):
+        self.tb_emps.setRowCount(0)
+        for i, e in enumerate(self.emps):
+            self.tb_emps.insertRow(i)
+            for j, value in enumerate(e.__dict__.values()):
+                self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            dept = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != dept.dept_id:
+                    self.tb_emps.hideRow(i)
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
