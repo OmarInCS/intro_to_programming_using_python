5e8f44eb3f0ea5f6853dacd0541926bda0a84225
OmarInCS <omarincs@gmail.com>
2021-03-22 19:40:39 +0300

    HR System

--- hr_system/controller/app.py
+++ hr_system/controller/app.py
@@ -1,3 +1,4 @@
+from datetime import date
 
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
@@ -23,6 +24,8 @@ class MainWindow(QWidget, Ui_Form):
         self.le_search.textChanged.connect(self.filter_emps_by_name)
         self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
         self.bt_del_dept.clicked.connect(self.delete_dept)
+        self.bt_add_emp.clicked.connect(self.show_add_emp_dialog)
+        self.bt_del_emp.clicked.connect(self.delete_emp)
 
 
     def load_depts(self):
@@ -72,6 +75,35 @@ class MainWindow(QWidget, Ui_Form):
             self.cb_depts.removeItem(idx)
             dept = self.depts.pop(idx - 1)
             dept.delete_from_db()
+
+    def show_add_emp_dialog(self):
+        dialog = loadUi("../view/add_emp.ui")
+        dialog.de_hire_date.setDate(date.today())
+        jobs = {str(e.job_id) for e in self.emps}
+        dialog.cb_jobs.addItems(jobs)
+        depts = [d.dept_name for d in self.depts]
+        dialog.cb_depts.addItems(depts)
+        choice = dialog.exec()
+
+        if choice == 1:
+            idx = dialog.cb_depts.currentIndex()
+            emp = Employee(dialog.le_emp_id.text(),
+                           dialog.le_emp_name.text(),
+                           dialog.le_email.text(),
+                           dialog.de_hire_date.date().toString("yyyy-MM-dd"),
+                           dialog.cb_jobs.currentText(),
+                           dialog.le_salary.text(),
+                           self.depts[idx - 1].dept_id)
+            self.emps.append(emp)
+            self.load_emps()
+            emp.save_to_db()
+
+    def delete_emp(self):
+        idx = self.tb_emps.currentRow()
+        if idx != -1:
+            self.tb_emps.removeRow(idx)
+            emp = self.emps.pop(idx)
+            emp.delete_from_db()
 
 
 app = QApplication([])
