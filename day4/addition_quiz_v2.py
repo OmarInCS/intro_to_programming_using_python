dd61c887cf32820c8dd4f08503a8cd8c54bb50e5
OmarInCS <omarincs@gmail.com>
2021-04-23 07:09:25 +0300

    loops

---
+++ day4/addition_quiz_v2.py
@@ -0,0 +1,19 @@
+
+from random import randint
+
+x = randint(0, 10)
+y = randint(0, 10)
+
+print("What's", x, "+", y, "?")
+answer = eval(input("ans: "))
+
+while answer == x+y:
+    x = randint(0, 10)
+    y = randint(0, 10)
+
+    print("What's", x, "+", y, "?")
+    answer = eval(input("ans: "))
+
+print("Game Over!!")
+
+
---
+++ day4/loop_basics.py
@@ -0,0 +1,11 @@
+
+x = 10
+while x <= 100:
+    print(x, end="\t")
+    x += 10
+
+print()
+print("-" * 20)
+
+for y in range(10, 101, 10):
+    print(y, end="\t")
