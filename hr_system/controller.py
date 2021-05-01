84586ea8b12947603dc45776d65e4e610db13965
OmarInCS <omarincs@gmail.com>
2021-05-02 00:26:13 +0300

    HR System

--- hr_system/controller.py
+++ hr_system/controller.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
 from hr_system.model.department import Department
 from hr_system.model.employee import Employee
@@ -17,18 +18,59 @@ class Controller(QWidget, Ui_Form):
         self.emps = Employee.get_all_emps()
         self.load_emps()
 
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+        self.le_search.textChanged.connect(self.filter_emps_by_name)
+        self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
+        self.bt_del_dept.clicked.connect(self.delete_dept)
+
 
     def load_depts(self):
         names = [d.dept_name for d in self.depts]
         self.cb_depts.addItems(names)
 
     def load_emps(self):
-
+        self.tb_emps.setRowCount(0)
         for i, e in enumerate(self.emps):
             self.tb_emps.insertRow(i)
             for j, value in enumerate(e.__dict__.values()):
                 self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
 
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            dept = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != dept.dept_id:
+                    self.tb_emps.hideRow(i)
+
+    def filter_emps_by_name(self):
+        self.load_emps()
+        search = self.le_search.text().lower()
+        if search != "":
+            for i, e in enumerate(self.emps):
+                if not e.emp_name.lower().startswith(search):
+                    self.tb_emps.hideRow(i)
+
+    def show_add_dept_dialog(self):
+        dialog = loadUi("view/add_dept.ui")
+        locs = {str(d.loc_id) for d in self.depts}
+        dialog.cb_locs.addItems(locs)
+        choice = dialog.exec()
+
+        if choice == 1:
+            dept = Department(dialog.le_dept_id.text(),
+                              dialog.le_dept_name.text(),
+                              dialog.cb_locs.currentText())
+            self.depts.append(dept)
+            self.cb_depts.addItem(dept.dept_name)
+            dept.save_to_db()
+
+    def delete_dept(self):
+        idx = self.cb_depts.currentIndex()
+        if idx != 0:
+            self.cb_depts.removeItem(idx)
+            dept = self.depts.pop(idx - 1)
+            dept.delete_from_db()
 
 
 app = QApplication([])
