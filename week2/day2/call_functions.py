4d17e6df660311d16fb46db3cc564b400a727b59
OmarInCS <omarincs@gmail.com>
2021-09-04 20:30:04 +0300

    nested loops & functions

---
+++ week2/day2/call_functions.py
@@ -0,0 +1,12 @@
+
+from week2.day2.my_functions import *
+
+# w, h = eval(input("Enter weight and height: "))
+# bmi = get_bmi(w, h)
+# status = get_status(bmi)
+# print(status, bmi)
+
+print(get_pct(23, 25))
+print(get_pct(23))
+print(get_pct())
+print(get_pct(fm=30))
