cdb6a9380fb57f12dfc2119847e877f96ad15392
OmarInCS <omarincs@gmail.com>
2021-09-14 19:48:54 +0300

    if-statment

--- week1/day3/check_day.py
+++ week1/day3/check_day.py
@@ -1,7 +1,8 @@
 
 day = eval(input("Enter a day number: "))
 
-if day >= 1 and day <= 5:
+# if day >= 1 and day <= 5:
+if 1 <= day <= 5:
     print("Work-day")
 elif day == 6 or day == 7:
     print("Off-day")
