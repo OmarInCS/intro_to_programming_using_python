4a691fcfbf8e46ae5a84b93ebab61cf818f9644d
OmarInCS <omarincs@gmail.com>
2021-09-25 20:46:32 +0300

    HR System

--- hr_system/app.py
+++ hr_system/app.py
@@ -1,6 +1,11 @@
+from datetime import date
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
+from hr_system.model.department import Department
+from hr_system.model.employee import Employee
 from hr_system.view.main_window import Ui_Form
 
 
@@ -9,6 +14,120 @@ class MyWidget(QWidget, Ui_Form):
     def __init__(self):
         QWidget.__init__(self)
         self.setupUi(self)
+
+        self.depts = Department.get_all_depts()
+        self.load_depts()
+
+        self.emps = Employee.get_all_emps()
+        self.load_emps()
+
+        self.tb_emps.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        self.cb_depts.currentIndexChanged.connect(self.filter_emps_by_dept)
+        self.le_search.textChanged.connect(self.filter_emps_by_name)
+        self.bt_add_dept.clicked.connect(self.show_add_dept_dialog)
+        self.bt_del_dept.clicked.connect(self.delete_dept)
+        self.bt_add_emp.clicked.connect(self.show_add_emp_dialog)
+        self.bt_del_emp.clicked.connect(self.delete_emp)
+        self.bt_export.clicked.connect(self.export_data)
+        self.bt_exit.clicked.connect(app.exit)
+
+    def load_depts(self):
+        names = [d.dept_name for d in self.depts]
+        self.cb_depts.addItems(names)
+
+    def load_emps(self):
+        self.tb_emps.setRowCount(0)
+        for i, e in enumerate(self.emps):
+            self.tb_emps.insertRow(i)
+            for j, value in enumerate(e.__dict__.values()):
+                self.tb_emps.setItem(i, j, QTableWidgetItem(str(value)))
+
+    def filter_emps_by_dept(self, idx):
+        self.load_emps()
+        if idx != 0:
+            dept = self.depts[idx - 1]
+            for i, e in enumerate(self.emps):
+                if e.dept_id != dept.dept_id:
+                    self.tb_emps.hideRow(i)
+
+    def filter_emps_by_name(self):
+        self.load_emps()
+        query = self.le_search.text().lower()
+        for i, e in enumerate(self.emps):
+            if not e.emp_name.lower().startswith(query):
+                self.tb_emps.hideRow(i)
+
+    def show_add_dept_dialog(self):
+        dialog = loadUi("view/add_dept.ui")
+        locs = {str(d.loc_id) for d in self.depts}
+        dialog.cb_locs.addItems(locs)
+        choice = dialog.exec()
+
+        if choice == 1:
+            dept = Department(dialog.le_dept_id.text(),
+                              dialog.le_dept_name.text(),
+                              dialog.cb_locs.currentText())
+            self.depts.append(dept)
+            self.cb_depts.addItem(dept.dept_name)
+            dept.save_to_db()
+
+    def delete_dept(self):
+        idx = self.cb_depts.currentIndex()
+        if idx != 0:
+            self.cb_depts.removeItem(idx)
+            dept = self.depts.pop(idx - 1)
+            dept.delete_from_db()
+
+    def show_add_emp_dialog(self):
+        dialog = loadUi("view/add_emp.ui")
+        dialog.de_hire_date.setDate(date.today())
+        jobs = {str(e.job_id) for e in self.emps}
+        dialog.cb_jobs.addItems(jobs)
+        depts = [d.dept_name for d in self.depts]
+        dialog.cb_depts.addItems(depts)
+        choice = dialog.exec()
+
+        if choice == 1:
+            idx = dialog.cb_depts.currentIndex()
+            emp = Employee(dialog.le_emp_id.text(),
+                           dialog.le_emp_name.text(),
+                           dialog.le_email.text(),
+                           dialog.de_hire_date.date().toString("yyyy-MM-dd"),
+                           dialog.cb_jobs.currentText(),
+                           dialog.le_salary.text(),
+                           self.depts[idx - 1].dept_id)
+            self.emps.append(emp)
+            self.load_emps()
+            emp.save_to_db()
+
+    def delete_emp(self):
+        idx = self.tb_emps.currentRow()
+        if idx != -1:
+            self.tb_emps.removeRow(idx)
+            emp = self.emps.pop(idx)
+            emp.delete_from_db()
+
+    def export_data(self):
+        info = QFileDialog().getSaveFileName(filter="CSV File (*.csv)")
+
+        if info[0] != "":
+            with open(info[0], "w") as file:
+                col_count = self.tb_emps.columnCount()
+                row_count = self.tb_emps.rowCount()
+
+                for i in range(col_count):
+                    header = self.tb_emps.horizontalHeaderItem(i).text()
+                    file.write(header + ",")
+                file.write("\n")
+
+                for i in range(row_count):
+                    if not self.tb_emps.isRowHidden(i):
+                        for j in range(col_count):
+                            data = self.tb_emps.item(i, j).text()
+                            file.write(data + ",")
+                        file.write("\n")
+
 
 
 app = QApplication([])
---
+++ hr_system/model/department.py
@@ -0,0 +1,40 @@
+import os
+from sqlite3.dbapi2 import connect
+
+
+class Department:
+
+    def __init__(self, dept_id, dept_name, loc_id):
+        self.dept_id = dept_id
+        self.dept_name = dept_name
+        self.loc_id = loc_id
+
+    def __repr__(self):
+        return self.dept_name
+
+    @staticmethod
+    def get_all_depts():
+        db_path = f"{os.getcwd()}\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "SELECT * FROM departments"
+            result = cur.execute(sql).fetchall()
+            result = [Department(*row) for row in result]
+
+            return result
+
+    def save_to_db(self):
+        db_path = f"{os.getcwd()}\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "INSERT INTO departments VALUES (:dept_id, :dept_name, :loc_id)"
+            cur.execute(sql, self.__dict__)
+            conn.commit()
+
+    def delete_from_db(self):
+        db_path = f"{os.getcwd()}\\hr.db"
+        with connect(db_path) as conn:
+            cur = conn.cursor()
+            sql = "DELETE FROM departments WHERE department_id = :dept_id"
+            cur.execute(sql, self.__dict__)
+            conn.commit()
