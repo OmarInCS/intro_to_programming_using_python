2ad226885325a219e417cf1fb80780728f35c35f
OmarInCS <omarincs@gmail.com>
2021-11-02 18:10:15 +0300

    for-loop

---
+++ week2/day2/quiz_v4.py
@@ -0,0 +1,19 @@
+
+from random import randint
+
+n = eval(input("Enter number of questions: "))
+
+score = 0
+for i in range(n):
+    x = randint(0, 10)
+    y = randint(0, 10)
+    answer = eval(input(f"What's {x} * {y} ? "))
+
+    if answer == x*y:
+        print("Correct Answer")
+        score += 1
+    else:
+        print("Wrong Answer")
+
+print("Your score:", score, "out of", n)
+
