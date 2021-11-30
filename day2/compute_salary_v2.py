0fc495889dd41df3cb4cdb689173b075cc385d16
OmarInCS <omarincs@gmail.com>
2021-11-30 17:40:49 +0300

    if-statment

---
+++ day2/compute_salary_v2.py
@@ -0,0 +1,14 @@
+
+salary = eval(input("Enter a salary: "))
+annual_salary = salary * 12
+print("Annual Salary:", annual_salary, "SAR")
+
+if salary > 12000:
+    print("High Salary")
+elif salary > 6000:
+    print("Normal Salary")
+elif salary > 3000:
+    print("Low Salary")
+else:
+    print("Very Low Salary")
+
