6e466725139bb20190635dc69fa2b2c6fd5cc56b
OmarInCS <omarincs@gmail.com>
2022-01-16 17:39:37 +0300

    loops

---
+++ week2/day1/guess_game.py
@@ -0,0 +1,12 @@
+from random import randint
+
+number = randint(0, 100)
+guess = eval(input("Guess a num. btw 0-100: "))
+
+while guess != number:
+    if guess > number:
+        guess = eval(input("Go Down: "))
+    else:
+        guess = eval(input("Go Up: "))
+
+print("You Guessed Right!!")
