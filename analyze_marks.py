ad1de8b527ed41de6e86b7e848649d6da818391a
OmarInCS <omarincs@gmail.com>
2022-05-10 17:57:33 +0300

    week 1, day 1

---
+++ analyze_marks.py
@@ -0,0 +1,9 @@
+# 0599888921
+m1, m2, m3 = eval(input("Enter three marks: "))
+
+avg = (m1 + m2 + m3) / 3
+
+print("Average mark:", round(avg, 2))
+print("Best mark:", max(m1, m2, m3))
+print("Worst mark:", min(m1, m2, m3))
+
---
+++ compute_percent.py
@@ -0,0 +1,5 @@
+
+full_mark = eval(input("Enter exam full mark: "))
+mark = eval(input("Enter your mark: "))
+pct = mark / full_mark * 100
+print("Percent:", pct, "%")
