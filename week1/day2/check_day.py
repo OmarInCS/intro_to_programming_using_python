056de662514bf3415a3f3a320d506b63a0e26a59
OmarInCS <omarincs@gmail.com>
2022-05-10 19:54:45 +0300

    week 1, day 2

---
+++ week1/day2/check_day.py
@@ -0,0 +1,9 @@
+
+day = eval(input("Enter a day number: "))
+
+if day >= 1 and day <= 5:
+    print("Work-day")
+elif day == 6 or day == 7:
+    print("Off-day")
+else:
+    print("Invalid Day Number")
