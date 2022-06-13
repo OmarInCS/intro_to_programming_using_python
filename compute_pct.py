7087795b00af8ecec577b0aa5c0da2c9ba4e8003
OmarInCS <omarincs@gmail.com>
2022-06-13 20:26:43 +0300

    input and variables

---
+++ compute_pct.py
@@ -0,0 +1,5 @@
+
+mark = eval(input("Enter your mark: "))
+full_mark = eval(input("Enter exam full mark: "))
+pct = mark / full_mark * 100
+print("Percent:", pct, "%")
---
+++ compute_salary.py
@@ -0,0 +1,4 @@
+
+salary = eval(input("Enter your salary: "))
+annual_salary = salary * 12
+print("Annual salary is:", annual_salary, "SAR")
